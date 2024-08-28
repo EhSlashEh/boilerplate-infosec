87aeb1efb026a7a3d2db1278a062e9e14d55eb72
EhSlashEh <anthonyalanb@hotmail.com>
2024-08-28 20:50:11 -0300

    Hide Power

--- myApp.js
+++ myApp.js
@@ -4,6 +4,9 @@ const app = express();
 
 app.use(helmet());
 
+// Hide the X-Powered-By header
+app.use(helmet.hidePoweredBy());
+
 module.exports = app;
 const api = require('./server.js');
 app.use(express.static('public'));
