e5af14755fad74820ea1ba359335a976c03cf679
EhSlashEh <anthonyalanb@hotmail.com>
2024-08-28 22:59:43 -0300

    More helmet stuff

--- myApp.js
+++ myApp.js
@@ -2,34 +2,43 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-// Use Helmet to secure the app
 app.use(helmet());
 
-// Hide the X-Powered-By header
 app.use(helmet.hidePoweredBy());
 
-// Prevent the app from being embedded in an iframe
 app.use(helmet.frameguard({ action: 'deny' }));
 
-// Import and use the API routes
+app.use(helmet.xssFilter());
+
+app.use(helmet.noSniff());
+
+app.use(helmet.ieNoOpen());
+
+app.use(helmet.hsts({
+  maxAge: 90 * 24 * 60 * 60,
+  force: true
+}));
+
+app.use(helmet.dnsPrefetchControl());
+
+app.use(helmet.noCache());
+
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", 'trusted-cdn.com']
+  }
+}));
+
+module.exports = app;
 const api = require('./server.js');
-app.use('/_api', api);
-
-// Serve static files
 app.use(express.static('public'));
-
-// Disable strict-transport-security header
 app.disable('strict-transport-security');
-
-// Define the route for the root
+app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-
-// Set the port and start the server
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
 });
-
-module.exports = app;
